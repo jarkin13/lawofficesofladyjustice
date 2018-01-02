e78cc9dc416be1ad88dd24b8e9eee1cb3b9d2afe
Jamie Arkin <jamielaurenarkin@gmail.com>
2018-01-02 18:00:28 -0500

    fix styling for social list

--- wp-content/themes/mesmerize/template-parts/navigation/navigation.php
+++ wp-content/themes/mesmerize/template-parts/navigation/navigation.php
@@ -6,22 +6,18 @@
 	        </div>
 	        <div class="main_menu_col col-xs">
 	            <?php mesmerize_print_primary_menu(); ?>
-	        </div>
+              <ul class="social-list">
+                <li>
+                  <a href="https://www.facebook.com/Law-Offices-Of-Lady-Justice-131718516879377/" target="_blank"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
+                </li>
+                <li>
+                  <a href="https://www.yelp.com/biz/law-offices-of-lady-justice-victorville-2" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+                </li>
+                <li>
+                  <a href="https://www.linkedin.com/in/lawofficesofladyjustice/" target="_blank"><i class="fa fa-yelp" aria-hidden="true"></i></a>
+                </li>
+              </ul>
+          </div>
 	    </div>
-      <div class="row basic-auto">
-        <div class="social-col col-12">
-          <ul>
-            <li>
-              <a href="https://www.facebook.com/Law-Offices-Of-Lady-Justice-131718516879377/" target="_blank"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
-            </li>
-            <li>
-              <a href="https://www.yelp.com/biz/law-offices-of-lady-justice-victorville-2" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
-            </li>
-            <li>
-              <a href="https://www.linkedin.com/in/lawofficesofladyjustice/" target="_blank"><i class="fa fa-yelp" aria-hidden="true"></i></a>
-            </li>
-          </ul>
-        </div>
-      </div>
     </div>
 </div>
